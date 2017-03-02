a278c427a387eb351704406f89e0995338836de1
Gen1us2k <minkin.andrew@gmail.com>
2017-03-02 18:25:56 +0600

    Added code from 5h step

---
+++ step06/storage/storage.go
@@ -0,0 +1,24 @@
+package storage
+
+import (
+	"sync"
+
+	"github.com/maddevsio/gocodelabru/step06/lru"
+)
+
+type (
+	Location struct {
+		Lat float64
+		Lon float64
+	}
+	Driver struct {
+		ID           int
+		LastLocation Location
+		Locations    *lru.LRU
+	}
+	DriverStorage struct {
+		mu        *sync.RWMutex
+		drivers   map[int]Driver
+		locations *rtreego.Rtree
+	}
+)
