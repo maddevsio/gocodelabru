b5c3f3394c896366c935a48730409892bb1669db
Gen1us2k <minkin.andrew@gmail.com>
2017-03-23 11:39:06 +0600

    Latest code

--- step08/api/api.go
+++ step08/api/api.go
@@ -1,62 +1,102 @@
 package api
 
 import (
-	"sync"
-	"time"
+	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
-	"github.com/maddevsio/gocodelabru/step07/storage"
 )
 
-type DBAPI struct {
-	database  *storage.DriverStorage
-	waitGroup sync.WaitGroup
-	echo      *echo.Echo
-	bindAddr  string
+type API struct {
+	echo     *echo.Echo
+	bindAddr string
 }
 
-func New(bindAddr string, lruSize int) *DBAPI {
-	a := &DBAPI{}
-	a.database = storage.New(lruSize)
+func New(bindAddr string) *API {
+	a := &API{}
 	a.echo = echo.New()
+	a.bindAddr = bindAddr
 	g := a.echo.Group("/api")
 	g.POST("/driver/", a.addDriver)
 	g.GET("/driver/:id", a.getDriver)
 	g.DELETE("/driver/:id", a.deleteDriver)
 	g.GET("/driver/:lat/:lon/nearest", a.nearestDrivers)
-	a.bindAddr = bindAddr
 	return a
 }
 
-func (a *DBAPI) addDriver(c echo.Context) error {
-	return nil
-}
-func (a *DBAPI) getDriver(c echo.Context) error {
-	return nil
-}
-func (a *DBAPI) deleteDriver(c echo.Context) error {
-	return nil
-}
-func (a *DBAPI) nearestDrivers(c echo.Context) error {
-	return nil
+func (a *API) Start() error {
+	return a.echo.Start(a.bindAddr)
 }
 
-func (a *DBAPI) Start() {
-	a.waitGroup.Add(1)
-	go func() {
-		a.echo.Start(a.bindAddr)
-		a.waitGroup.Done()
-	}()
-	a.waitGroup.Add(1)
-	go a.removeExpired()
-}
-
-func (a *DBAPI) removeExpired() {
-	for range time.Tick(1) {
-		a.database.DeleteExpired()
+func (a *API) addDriver(c echo.Context) error {
+	p := &Payload{}
+	if err := c.Bind(p); err != nil {
+		return c.JSON(http.StatusUnsupportedMediaType, &DefaultResponse{
+			Success: false,
+			Message: "Set content-type application/json or check your payload data",
+		})
 	}
+	return c.JSON(http.StatusOK, &DefaultResponse{
+		Success: false,
+		Message: "Added",
+	})
+}
+func (a *API) getDriver(c echo.Context) error {
+	driverID := c.Param("id")
+	id, err := strconv.Atoi(driverID)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, &DefaultResponse{
+			Success: false,
+			Message: "could not convert string to integer",
+		})
+	}
+	return c.JSON(http.StatusOK, &DriverResponse{
+		Success: true,
+		Message: "found",
+		Driver:  id,
+	})
 }
 
-func (a *DBAPI) WaitStop() {
-	a.waitGroup.Wait()
+func (a *API) deleteDriver(c echo.Context) error {
+	driverID := c.Param("id")
+	_, err := strconv.Atoi(driverID)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, &DefaultResponse{
+			Success: false,
+			Message: "could not convert string to integer",
+		})
+	}
+	return c.JSON(http.StatusOK, &DefaultResponse{
+		Success: true,
+		Message: "removed",
+	})
+}
+func (a *API) nearestDrivers(c echo.Context) error {
+	lat := c.Param("lat")
+	lon := c.Param("lon")
+	if lat == "" || lon == "" {
+		return c.JSON(http.StatusBadRequest, &DefaultResponse{
+			Success: false,
+			Message: "empty coordinates",
+		})
+	}
+	_, err := strconv.ParseFloat(lat, 64)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, &DefaultResponse{
+			Success: false,
+			Message: "failed convert float",
+		})
+	}
+	_, err = strconv.ParseFloat(lon, 64)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, &DefaultResponse{
+			Success: false,
+			Message: "failed convert float",
+		})
+	}
+	// TODO: Add nearest
+	return c.JSON(http.StatusOK, &NearestDriverResponse{
+		Success: false,
+		Message: "found",
+	})
 }
--- step08/api/models.go
+++ step08/api/models.go
@@ -1,7 +1,5 @@
 package api
 
-import "github.com/maddevsio/gocodelabru/step07/storage"
-
 type (
 	Location struct {
 		Latitude  float64 `json:"lat"`
@@ -12,18 +10,21 @@ type (
 		DriverID  int      `json:"driver_id"`
 		Location  Location `json:"location"`
 	}
+	// Структура для возврата ответа по умолчанию
 	DefaultResponse struct {
 		Success bool   `json:"success"`
 		Message string `json:"message"`
 	}
+	// Для возврата ответа, когда мы запрашиваем водителя
 	DriverResponse struct {
-		Success bool            `json:"success"`
-		Message string          `json:"message"`
-		Driver  *storage.Driver `json:"driver"`
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+		Driver  int    `json:"driver"`
 	}
+	// Для возврата ближайших водителей
 	NearestDriverResponse struct {
-		Success bool              `json:"success"`
-		Message string            `json:"message"`
-		Drivers []*storage.Driver `json:"drivers"`
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+		Drivers []int  `json:"drivers"`
 	}
 )
