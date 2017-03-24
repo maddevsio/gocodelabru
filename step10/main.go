9ac8821051524aba0ac28caea7debb8044148269
Gen1us2k <minkin.andrew@gmail.com>
2017-03-24 12:43:28 +0600

    Code

--- step10/main.go
+++ step10/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"flag"
+	"log"
 
-	"github.com/maddevsio/gocodelabru/step09/api"
+	"github.com/maddevsio/gocodelabru/step06/api"
 )
 
 func main() {
+
 	bindAddr := flag.String("bind_addr", ":8080", "Set bind address")
-	size := flag.Int("lru_size", 20, "Set lru size per driver")
 	flag.Parse()
-	a := api.New(*bindAddr, *size)
-	a.Start()
-	a.WaitStop()
+
+	a := api.New(*bindAddr)
+	log.Fatal(a.Start())
 }
